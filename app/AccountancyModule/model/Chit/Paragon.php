b94ba0c8cac6d247eefabe57f068fa84b4fa7a1f
František Hána <sinacek@gmail.com>
2012-02-25 18:00:02 +0100

    prejmenovane sablony + dodelavka modelu

---
+++ app/AccountancyModule/model/Chit/Paragon.php
@@ -0,0 +1,33 @@
+<?php
+class Paragon extends Object {
+
+    public $date; //datum 
+    public $komu; //vyplaceno komu
+    public $ucel; //ucela vyplaty
+    public $price; // castka na paragonu
+    public $priceText; // vyraz na spocitani paragonu
+    public $type;   //prijmovy, potraviny, atd
+    public $cislo; // siclo v pokladni knize
+
+
+    /**
+     *
+     * @param ArrayHash $values
+     */
+    function  __construct($values = array()) {
+        if(isset($values['price'])) {
+            $price = $values['price'];
+            if(preg_match("/^[0-9]*\.[0-9]$/", $values['price']))
+                $price .= "0";
+        }
+
+        $this->date = $values['date'];
+        $this->komu = isset($values['komu']) ? $values['komu'] : "";
+        $this->ucel = isset($values['ucel']) ? $values['ucel'] : "";
+        $this->price = isset($price) ? $price : "0";
+        $this->priceText = isset($values['priceText']) ? $values['priceText'] : $values['price'];
+        $this->type = isset($values['type']) ? $values['type'] : "";
+        $this->cislo = isset($values['cislo']) ? $values['cislo'] : "";
+    }
+
+}
