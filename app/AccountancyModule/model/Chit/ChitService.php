89b801a717abc62699539f5f1d46695700f8b78b
František Hána <sinacek@gmail.com>
2012-02-25 18:00:02 +0100

    prejmenovane sablony + dodelavka modelu
    Former-commit-id: b94ba0c8cac6d247eefabe57f068fa84b4fa7a1f

---
+++ app/AccountancyModule/model/Chit/ChitService.php
@@ -0,0 +1,66 @@
+<?php
+
+class ChitService extends BaseService {
+    
+    public function __construct() {
+        parent::__construct();
+        $this->table = new ChitTable();
+    }
+    
+    public function getAll($actionId){
+        return $this->table->getAll($actionId);
+    }
+    
+    
+    public function add($actionId, $v){
+        if(!is_array($v) && !($v instanceof Traversable))
+            throw new InvalidArgumentException("Values nejsou ve správném formátu");
+        //@todo kontrola jestli ma pravo přidávat k $actionId
+        
+        $values = array(
+            "actionId" => $actionId,
+            "date" => $v['date'],
+            "recipient" => $v['recipient'],
+            "purpose" => $v['purpose'],
+            "price" => $v['price'],
+            "priceText" => $v['priceText'],
+            "category" => $v['type'],
+        );
+        
+        return $this->table->add($values);
+    }
+    
+    public function delete($id, $actionId){
+        //@todo kontrola jestli ma pravo mazat $actionId
+        return $this->table->delete($id, $actionId);
+    }
+    
+    /**
+     * vrací prijmové kategorie
+     * @return type 
+     */
+    public function getCategoriesIn(){
+        return $this->table->getCategories("in");
+        
+    }
+    
+    /**
+     * vrací výdajové kategorie
+     * @return type 
+     */
+    public function getCategoriesOut(){
+        return $this->table->getCategories("out");
+    }
+    
+    
+    /**
+     * je akce v záporu?
+     * @param type $actionId
+     * @return bool
+     */
+    public function isInMinus($actionId){
+        return $this->table->isInMinus($actionId);
+    }
+    
+    
+}
---
+++ app/AccountancyModule/model/Chit/ChitTable.php
@@ -0,0 +1,41 @@
+<?php
+class ChitTable extends BaseTable {
+    public function getAll($actionId){
+        return dibi::fetchAll("SELECT ch.*, cat.label as clabel, cat.short as cshort, cat.type as ctype FROM [".self::TABLE_CHIT."] as ch
+            LEFT JOIN [".self::TABLE_CATEGORY."] as cat ON (ch.category = cat.id) 
+                WHERE actionId=%i AND ch.deleted = 0
+                ORDER BY ch.date, ctype ", $actionId);
+    }
+    
+    public function add($values){
+        return dibi::query("INSERT INTO [".self::TABLE_CHIT."] %v", $values);
+    }
+    
+    public function delete($id, $actionId){
+        return dibi::query("UPDATE [".self::TABLE_CHIT."] SET deleted=1 WHERE id = %i AND actionID = %i LIMIT 1", $id, $actionId);
+    }
+    
+    /**
+     * vrací seznam kategorií
+     * @param type $type
+     * @return type 
+     */
+    public function getCategories($type = NULL){
+        return dibi::fetchPairs("SELECT id, label FROM [".self::TABLE_CATEGORY."] WHERE deleted = 0 %if", isset($type), " AND type=%s %end", $type);
+    }
+    
+    /**
+     * spočítá příjmy a výdaje a ty pak odečte
+     * @param type $actionId
+     * @return type 
+     */
+    public function isInMinus($actionId) {
+        $data = dibi::fetchPairs("SELECT cat.type, SUM(ch.price) as sum FROM ac_chits as ch
+            LEFT JOIN ac_category as cat ON (ch.category = cat.id) 
+            WHERE ch.actionId = %i AND ch.deleted =0
+            GROUP BY cat.type", $actionId);
+        
+        return @($data["in"] - $data["out"]) < 0 ? true : false; // @ aby neházelo chybu kdyz chybí jedna z polozek
+    }
+    
+}
