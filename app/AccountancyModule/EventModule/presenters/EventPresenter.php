3f0710e9d6e12d36ce52a13bd18d991c98cbb0b5
Hána František <sinacek@gmail.com>
2013-01-28 23:33:59 +0100

    nastaveni limitu pro generování celých pdf

--- app/AccountancyModule/EventModule/presenters/EventPresenter.php
+++ app/AccountancyModule/EventModule/presenters/EventPresenter.php
@@ -94,6 +94,11 @@ class Accountancy_Event_EventPresenter extends Accountancy_Event_BasePresenter {
         $template .= (string)$this->context->exportService->getCashbook($aid, $this->context->eventService) . $this->context->exportService->getNewPage();
         $template .= (string)$this->context->exportService->getChits($aid, $this->context->eventService, $this->context->unitService, $chits);
         
+//        echo $template;
+//        $size = memory_get_usage();
+//        $unit=array('b','kb','mb','gb','tb','pb');
+//        echo @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
+        
         $this->context->eventService->participants->makePdf($template, "all.pdf");
         $this->terminate();
     }
