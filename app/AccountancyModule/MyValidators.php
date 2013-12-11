8f905cf0e251e59a8f310473a4ce136d5a1897d7
sinacek <sinacek@gmail.com>
2013-12-11 22:31:38 +0100

    *vylepšená inline editace, +live validate, +přetahávání účastníků, +kontrola platnosti data při zakládání akce
    + vylepšení JS pro přetahování
    
    Signed-off-by: sinacek <sinacek@gmail.com>
    
    Former-commit-id: 2c164311bfe77706047eb03c59afb4a253d2e860

---
+++ app/AccountancyModule/MyValidators.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ *
+ * @author Sin
+ */
+class MyValidators
+{
+    public static function isValidDate($control /*, $arg]*/) {
+        return $control->value === NULL ? FALSE : TRUE;
+    }
+}
