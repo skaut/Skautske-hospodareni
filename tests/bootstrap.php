3091d4335f38384099a657a858bee5f1eb75bf48
sinacek <sinacek@gmail.com>
2014-05-12 15:12:44 +0200

    +základ Nette Tester
    Signed-off-by: sinacek <sinacek@gmail.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,47 +1,18 @@
 <?php
 
-define('WWW_DIR', dirname(__FILE__)."/..");
-define('APP_DIR', WWW_DIR . '/app');
-define('LIBS_DIR', WWW_DIR . '/libs');
-define('TEMP_DIR', APP_DIR . '/temp');
-define('TEST_DIR', WWW_DIR . '/tests');
+require __DIR__ . '/../vendor/autoload.php';
 
-require LIBS_DIR . '/Nette/loader.php';
+Tester\Environment::setup();
 
-// Configure application
-$configurator = new Configurator;
+class_alias('Tester\Assert', 'Assert');
+date_default_timezone_set('Europe/Prague');
 
-$configurator->setTempDirectory(APP_DIR. '/temp');
-$configurator->enableDebugger(APP_DIR . '/log', "sinacek@gmail.com");
+define('TEMP_DIR', __DIR__.'/tmp/'.getmypid());
 
-$configurator->addConfig(APP_DIR . '/config.neon', "console");
+Tester\Helpers::purge(TEMP_DIR);
 
-$configurator->createRobotLoader()
-    ->addDirectory(APP_DIR)
-    ->addDirectory(LIBS_DIR)
-    ->addDirectory(TEST_DIR)
+$loader = new Nette\Loaders\RobotLoader;
+$loader->addDirectory(__DIR__  . '/../app');
+$loader->setCacheStorage(new Nette\Caching\Storages\FileStorage(TEMP_DIR));
+$loader->register();
 
-    ->register();
-
-$container = $configurator->createContainer();
-
-// Setup router
-$router = $container->router;
-$router[] = new Route('index.php', ':Default:default', Route::ONE_WAY);
-$router[] = new Route('sign/<action>[/back-<backlink>]', array(
-    "presenter" => "Auth",
-    "action" => "default",
-    "backlink" => NULL
-));
-Accountancy_BasePresenter::createRoutes($router);
-$router[] = new SimpleRouter('Default:default');
-
-// Configure and run the application!
-$application = $container->application;
-$application->catchExceptions = false;
-//$application->errorPresenter = 'Error';
- if (!Environment::isConsole()){
-   $application->run();
- } else {
-     Environment::getSession()->start();
- }
