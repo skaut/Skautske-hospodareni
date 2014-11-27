fe8bec390a422c1f4215632789df2dc08c056cb9
sinacek <sinacek@gmail.com>
2014-11-27 15:03:59 +0100

    *oprava texte v cestáku +jquery 1.8.2 - historická potřeba
    Signed-off-by: sinacek <sinacek@gmail.com>
    
    Former-commit-id: 6ec1096c10cdb4a8c6cedb1eed250a82edf770a9

--- app/presenters/DefaultPresenter.php
+++ app/presenters/DefaultPresenter.php
@@ -3,11 +3,12 @@
 namespace App;
 
 class DefaultPresenter extends BasePresenter {
+
     protected function startup() {
         parent::startup();
     }
-    
-    public function renderDefault($backlink = NULL){
+
+    public function renderDefault($backlink = NULL) {
         
     }
 
