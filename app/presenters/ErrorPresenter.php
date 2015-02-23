4d116c4f44cd8d6ed1ff860ecce0ce0384a45cb7
sinacek <sinacek@gmail.com>
2015-02-23 15:08:26 +0100

    *oprava error presenteru
    Signed-off-by: sinacek <sinacek@gmail.com>
    
    Former-commit-id: b2f2d8ff2464a4607b51211f1051e55a54ece73f

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -28,10 +28,10 @@ class ErrorPresenter extends \Nette\Application\UI\Presenter {
             $code = $exception->getCode();
             // load template 403.latte or 404.latte or ... 4xx.latte
             $this->setView(in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx');
-        } elseif ($exception instanceof \SkautIS\Exception\PermissionException) {
+        } elseif ($exception instanceof \Skautis\Wsdl\PermissionException) {
             $this->flashMessage($exception->getMessage(), "danger");
             $this->redirect(":Default:");
-        } elseif ($exception instanceof \SkautIS\Exception\AuthenticationException) {//vypršelo přihlášení do SkautISu
+        } elseif ($exception instanceof \Skautis\Wsdl\AuthenticationException) {//vypršelo přihlášení do SkautISu
             $this->user->logout(TRUE);
             $this->flashMessage("Vypršelo přihlášení do skautISu", "danger");
             $this->redirect(":Default:");
