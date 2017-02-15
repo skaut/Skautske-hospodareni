6c9b2a8f2f48b3be6304668c6771fd22cde23207
Frantisek Masa <frantisekmasa1@gmail.com>
2017-02-16 00:29:00 +0100

    Removed constants.

--- app/model/Services/PdfRenderer.php
+++ app/model/Services/PdfRenderer.php
@@ -5,13 +5,6 @@ namespace Model\Services;
 class PdfRenderer
 {
 
-    private const ORIENTATION = 'P';
-    private const FONT_SIZE = 0;
-    private const FONT = '';
-    private const MARGIN = 10;
-    private const HEADER_MARGIN = 9;
-    private const FOOTER_MARGIN = 9;
-
     /**
      * Renders PDF to output stream
      * @param string $template
@@ -19,19 +12,7 @@ class PdfRenderer
      */
     public function render(string $template, string $filename) : void
     {
-        $mpdf = new \mPDF(
-            'utf-8',
-            'A4',
-            self::FONT_SIZE,
-            self::FONT,
-            self::MARGIN,
-            self::MARGIN,
-            self::MARGIN,
-            self::MARGIN,
-            self::HEADER_MARGIN,
-            self::FOOTER_MARGIN,
-            self::ORIENTATION
-        );
+        $mpdf = new \mPDF('utf-8', 'A4', 0, '',10,10, 10, 10, 9, 9, 'P');
 
         $mpdf->WriteHTML($template, NULL);
         $mpdf->Output($filename, 'I');
