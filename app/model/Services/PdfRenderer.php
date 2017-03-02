15b38c345931544ad4cd1cd853d418fea83902cb
František Maša <frantisekmasa1@gmail.com>
2017-03-02 23:41:30 +0100

    fixup! Updated mpdf to 7.0-dev.

--- app/model/Services/PdfRenderer.php
+++ app/model/Services/PdfRenderer.php
@@ -14,16 +14,16 @@ class PdfRenderer
      * @param string $filename
      * @param bool $landscape TRUE for landscape, FALSE for portrait mode
      */
-    public function render(string $template, string $filename, bool $landscape = FALSE): void
+    public function render(string $template, string $filename, bool $landscape = FALSE) : void
     {
-		$mpdf = new Mpdf([
-			'format' => $landscape ? 'A4-L' : 'A4',
-			'mode' => 'utf-8',
-			'margin_left' => 10,
-			'margin_right' => 10,
-			'margin_top' => 10,
-			'margin_bottom' => 10,
-		]);
+        $mpdf = new Mpdf([
+            'format' => $landscape ? 'A4-L' : 'A4',
+            'mode' => 'utf-8',
+            'margin_left' => 10,
+            'margin_right' => 10,
+            'margin_top' => 10,
+            'margin_bottom' => 10,
+        ]);
 
         $mpdf->WriteHTML($template, NULL);
         $mpdf->Output($filename, Destination::INLINE);
