ce32564bf48ea23a96995a9e176a34d9b82adb1b
František Maša <frantisekmasa1@gmail.com>
2017-03-09 12:24:16 +0100

    Auto-enabled csrf protection for all forms.

--- app/AccountancyModule/Forms/BaseForm.php
+++ app/AccountancyModule/Forms/BaseForm.php
@@ -2,9 +2,31 @@
 
 namespace App\Forms;
 
+use Nette;
 use Nette\Application\UI\Form;
+use Nette\Forms\Controls\CsrfProtection;
 
 class BaseForm extends Form
 {
     use ContainerTrait;
+
+    /** @var CsrfProtection */
+    private $protection;
+
+    public function __construct(Nette\ComponentModel\IContainer $parent = NULL, $name = NULL)
+	{
+		parent::__construct($parent, $name);
+		$this->protection = parent::addProtection('Vypršela platnost formuláře, zkus to ještě jednou.');
+	}
+
+	/**
+	 * @deprecated CSRF protection is auto-enabled for all forms
+	 * @param string|NULL $message
+	 * @return CsrfProtection
+	 */
+	public function addProtection($message = NULL) : CsrfProtection
+	{
+		return $this->protection;
+	}
+
 }
