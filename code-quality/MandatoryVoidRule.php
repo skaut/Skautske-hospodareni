e93dc3e34ef3a8a1faacc3311be0e3bae67bc086
František Maša <frantisekmasa1@gmail.com>
2017-03-26 19:30:45 +0200

    Added missing :void check
    Former-commit-id: 877802ff84b9da275ebf882944ce0fca301f9736

---
+++ code-quality/MandatoryVoidRule.php
@@ -0,0 +1,64 @@
+<?php
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Return_;
+use PhpParser\Node\Stmt\ClassMethod;
+use PHPStan\Analyser\Scope;
+use PHPStan\Type\VoidType;
+
+class MandatoryVoidRule implements \PHPStan\Rules\Rule
+{
+
+    public function getNodeType(): string
+    {
+        return ClassMethod::class;
+    }
+
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if(! ($node instanceof ClassMethod)) {
+            return [];
+        }
+
+        if($node->name === '__construct') {
+            return [];
+        }
+
+        $method = new ReflectionMethod($scope->getClass(), $node->name);
+
+        if($method->isAbstract()) {
+            return [];
+        }
+
+
+        if($this->returnsNullOrNothing($node) && $method->getReturnType() != 'void') {
+            return [
+                sprintf(
+                    'Void method %s::%s() doesn\'t use :void return type.',
+                    $scope->getClass(),
+                    $node->name
+                ),
+            ];
+        }
+        return [];
+    }
+
+    private function returnsNullOrNothing(ClassMethod $node)
+    {
+        $statements = $node->getStmts();
+
+        if($statements === NULL) {
+            return TRUE;
+        }
+
+        $traverser = new \PhpParser\NodeTraverser();
+
+        $visitor = new ReturnTypeVisitor();
+        $traverser->addVisitor($visitor);
+
+        $traverser->traverse($statements);
+
+        return $visitor->returnsNullOrNothing();
+    }
+
+}
