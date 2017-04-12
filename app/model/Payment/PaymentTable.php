80fb0984de4a46826bf04489dc94a7c222c4f705
František Maša <frantisekmasa1@gmail.com>
2017-04-12 18:39:56 +0200

    Remove unused methods

--- app/model/Payment/PaymentTable.php
+++ app/model/Payment/PaymentTable.php
@@ -13,26 +13,6 @@ class PaymentTable extends BaseTable
     const PAYMENT_STATE_PREPARING = "preparing";
     const PAYMENT_STATE_SEND = "send";
 
-    public function getSimple(int $paymentId) : Row
-    {
-        return $this->connection->select('*')
-            ->from(self::TABLE_PA_PAYMENT)
-            ->where('id = %i', $paymentId)
-            ->fetch();
-    }
-
-    /**
-     * @param int|NULL $pa_groups
-     * @return Row[]
-     */
-    public function getAllPayments($pa_groups)
-    {
-        return $this->connection->fetchAll("SELECT p.*, s.label as stateLabel FROM [" . self::TABLE_PA_PAYMENT . "] p "
-            . "LEFT JOIN [" . self::TABLE_PA_PAYMENT_STATE . "] s ON p.state = s.ID "
-            . "WHERE groupId IN %in ", $pa_groups, " "
-            . "ORDER BY s.orderby, p.name");
-    }
-
     /**
      * @param int $pa_groupId
      * @return array
@@ -40,16 +20,6 @@ class PaymentTable extends BaseTable
     public function getActivePaymentIds($pa_groupId)
     {
         return $this->connection->fetchPairs("SELECT id, personId FROM [" . self::TABLE_PA_PAYMENT . "] WHERE groupId=%i ", $pa_groupId, " AND state != 'canceled'");
-    }
-
-    /**
-     *
-     * @param array $arr
-     * @return bool
-     */
-    public function createPayment($arr): bool
-    {
-        return (bool)$this->connection->insert(self::TABLE_PA_PAYMENT, $arr)->execute();
     }
 
     /**
@@ -77,19 +47,6 @@ class PaymentTable extends BaseTable
     }
 
     /**
-     * @param int|int[] $unitId
-     * @param bool $onlyOpen
-     * @return array
-     */
-    public function getGroups($unitId, $onlyOpen): array
-    {
-        return $this->connection->query("SELECT * FROM [" . self::TABLE_PA_GROUP . "]"
-            . " WHERE unitId IN %in ", !is_array($unitId) ? [$unitId] : $unitId, " AND state", "%if ", $onlyOpen, "='open' %else !='canceled' %end"
-            . " ORDER BY id DESC")
-            ->fetchAssoc("id");
-    }
-
-    /**
      * @param string $groupType
      * @param int $sisId
      * @return Row[]
@@ -97,18 +54,6 @@ class PaymentTable extends BaseTable
     public function getGroupsBySisId($groupType, $sisId)
     {
         return $this->connection->fetchAll("SELECT * FROM [" . self::TABLE_PA_GROUP . "] WHERE groupType=%s ", $groupType, " AND sisId=%i ", $sisId, " AND state != 'canceled'");
-    }
-
-    /**
-     * @param int $pa_groupId
-     * @return array
-     */
-    public function summarizeByState($pa_groupId)
-    {
-        return $this->connection->query("SELECT s.label, SUM(amount) as amount, COUNT(p.id) as count FROM [" . self::TABLE_PA_PAYMENT . "] p"
-            . " RIGHT JOIN [" . self::TABLE_PA_PAYMENT_STATE . "] s ON p.state = s.ID AND groupId=%i", $pa_groupId, " WHERE s.id != 'canceled'"
-            . " GROUP BY s.id"
-            . " ORDER BY s.orderby")->fetchAssoc("label");
     }
 
     /**
