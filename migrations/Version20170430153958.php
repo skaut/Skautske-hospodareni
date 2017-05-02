d8019bd6277acdbbbbc64a2f0b842abcb7e1f579
František Maša <frantisekmasa1@gmail.com>
2017-05-02 18:36:18 +0200

    fixup! Create travel via entity

--- migrations/Version20170430153958.php
+++ migrations/Version20170430153958.php
@@ -11,7 +11,7 @@ class Version20170430153958 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql("ALTER TABLE tc_travels ADD has_fuel TINYINT(1) NOT NULL");
-        $this->addSql("UPDATE travels SET travels.has_fuel = types.hasFuel FROM tc_travels as travels INNER JOIN tc_travelTypes as types ON travels.type = types.type");
+        $this->addSql("UPDATE tc_travels INNER JOIN tc_travelTypes AS types ON tc_travels.type = types.type SET tc_travels.has_fuel = types.hasFuel");
     }
 
     public function down(Schema $schema): void
