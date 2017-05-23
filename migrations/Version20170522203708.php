442fe35f9fcf90150b74a8fa8acd0bbc433aa3be
František Hána <sinacek@gmail.com>
2017-05-23 18:15:30 +0200

    remove comment

--- migrations/Version20170522203708.php
+++ migrations/Version20170522203708.php
@@ -5,22 +5,14 @@ namespace Migrations;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 class Version20170522203708 extends AbstractMigration
 {
-    /**
-     * @param Schema $schema
-     */
+
     public function up(Schema $schema)
     {
         $this->addSql("ALTER TABLE `ac_chits` CHANGE `purpose` `purpose` varchar(120) COLLATE 'utf8_czech_ci' NOT NULL AFTER `recipient`");
     }
 
-    /**
-     * @param Schema $schema
-     */
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
