5225ff00c4ae707adabed04bcdd9aa39d1b36b8e
František Maša <frantisekmasa1@gmail.com>
2017-06-02 14:23:17 +0200

    Replace AI key with composite keys

---
+++ migrations/Version20170602122002.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20170602122002 extends AbstractMigration
+{
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE tc_travels MODIFY id BIGINT UNSIGNED NOT NULL');
+        $this->addSql('ALTER TABLE tc_travels DROP PRIMARY KEY');
+        $this->addSql('ALTER TABLE tc_travels ADD PRIMARY KEY (id, command_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE tc_travels DROP PRIMARY KEY');
+        $this->addSql('ALTER TABLE tc_travels CHANGE id id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL');
+        $this->addSql('ALTER TABLE tc_travels ADD PRIMARY KEY (id)');
+    }
+
+}
