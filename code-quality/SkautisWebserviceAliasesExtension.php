1797f10bc0bea67290da02b736c56a9818cdc5ac
František Maša <frantisekmasa1@gmail.com>
2017-07-28 20:56:58 +0200

    Check magic Skautis properties with phpstan

---
+++ code-quality/SkautisWebserviceAliasesExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\Nette\NetteObjectPropertyReflection;
+use PHPStan\Reflection\PropertiesClassReflectionExtension;
+use PHPStan\Reflection\PropertyReflection;
+use PHPStan\Type\ObjectType;
+use Skautis\Skautis;
+use Skautis\Wsdl\WebServiceInterface;
+
+class SkautisWebserviceAliasesExtension implements PropertiesClassReflectionExtension
+{
+
+    private const ALIASES = [
+        "user",
+        "usr",
+        "org",
+        "app",
+        "event",
+        "events",
+    ];
+
+    public function hasProperty(ClassReflection $classReflection, string $propertyName): bool
+    {
+        return $classReflection->getName() === Skautis::class
+            && in_array($propertyName, self::ALIASES, TRUE);
+    }
+
+
+    public function getProperty(ClassReflection $classReflection, string $propertyName): PropertyReflection
+    {
+        return new NetteObjectPropertyReflection(
+            $classReflection,
+            new ObjectType(WebServiceInterface::class)
+        ); // It works pretty much same as Nette\Object properties, so...
+    }
+
+}
