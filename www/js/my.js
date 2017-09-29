e6e9c7bf42a687799859c7089ed298004c3fe073
František Maša <frantisekmasa1@gmail.com>
2017-09-29 19:28:52 +0200

    Re-enable events summary filtering

--- www/js/my.js
+++ www/js/my.js
@@ -58,7 +58,60 @@ $(document).ready(function () {
         }
     });
 
+    prepareGroupActions();
+
 });
+
+
+function prepareGroupActions() {
+    $('.datagrid').each(function() {
+        prepareGridGroupActions($(this));
+    });
+
+}
+
+function prepareGridGroupActions(grid) {
+    const buttons = [];
+    const groupActionsRow = grid.find('.row-group-actions th');
+    const groupActionsSubmit = groupActionsRow.find('[type="submit"]');
+
+    groupActionsRow.find('select').each(function() {
+        const select = $(this);
+
+        select.find('option').each(function () {
+            var option = $(this);
+
+            if(option.val() === '') {
+                return;
+            }
+
+            const btn = $('<input>')
+                .attr({
+                    'type': 'submit',
+                    'name': groupActionsSubmit.attr('name'),
+                    'class': 'btn btn-default btn-xs pull-right disabled',
+                    'value': option.html(),
+                });
+
+            buttons.push(btn);
+            btn.appendTo(groupActionsRow);
+
+            $(btn).click(function () {
+                select.val(option.val());
+            });
+        });
+    });
+
+    const $buttons = $(buttons);
+    const checkboxes = grid.find('[type="checkbox"]');
+
+    checkboxes.change(function() {
+        $buttons.each(function() {
+            this.toggleClass('disabled', !checkboxes.is(':checked'))
+        });
+    });
+
+}
 
 function jqCheckAll(id, name) {
     $("input[name^=" + name + "][type='checkbox']").prop('checked', $('#' + id).is(':checked'));
