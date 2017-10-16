c8e2b6821ace1eaea8cf61458db5e2ce24b48e32
František Maša <frantisekmasa1@gmail.com>
2017-10-16 15:10:17 +0200

    Remove phpdoc

--- app/model/Mail/MailService.php
+++ app/model/Mail/MailService.php
@@ -9,9 +9,6 @@ use Model\Payment\MailCredentials;
 use Model\Payment\MailCredentialsNotFound;
 use Model\Payment\Repositories\IMailCredentialsRepository;
 
-/**
- * @author Hána František
- */
 class MailService
 {
 
