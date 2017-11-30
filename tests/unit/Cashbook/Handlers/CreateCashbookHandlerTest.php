fd9feb2b762ef05f8a6b1ae3e00ec9a11adcf18a
František Maša <frantisekmasa1@gmail.com>
2017-12-01 00:11:58 +0100

    Add cashbook tests

--- tests/unit/Cashbook/Handlers/CreateCashbookHandlerTest.php
+++ tests/unit/Cashbook/Handlers/CreateCashbookHandlerTest.php
@@ -1,10 +1,30 @@
 <?php
 
-
 namespace Model\Cashbook\Handlers;
 
+use Mockery as m;
+use Model\Cashbook\Cashbook;
+use Model\Cashbook\Commands\Cashbook\CreateCashbook;
+use Model\Cashbook\Handlers\Cashbook\CreateCashbookHandler;
+use Model\Cashbook\Repositories\ICashbookRepository;
 
-class CreateCashbookHandlerTest
+class CreateCashbookHandlerTest extends \Codeception\Test\Unit
 {
+
+    public function test(): void
+    {
+        $repository = m::mock(ICashbookRepository::class);
+        $repository->shouldReceive('save')
+            ->once()
+            ->with(m::on(function(Cashbook $cashbook) {
+                return $cashbook->getId() === 10;
+            }));
+
+        $handler = new CreateCashbookHandler($repository);
+
+        $handler->handle(new CreateCashbook(10));
+
+        m::close();
+    }
 
 }
