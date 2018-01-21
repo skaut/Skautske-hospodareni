8bcc5058dfa4dd7eb97174f96f995c31298835ea
František Maša <frantisekmasa1@gmail.com>
2018-01-21 23:25:12 +0100

    Fix return type

--- app/model/Skautis/WebserviceFactory.php
+++ app/model/Skautis/WebserviceFactory.php
@@ -8,6 +8,7 @@ use Nette\StaticClass;
 use Skautis\Skautis;
 use Skautis\Wsdl\Decorator\Cache\ArrayCache;
 use Skautis\Wsdl\Decorator\Cache\CacheDecorator;
+use Skautis\Wsdl\WebService;
 
 final class WebserviceFactory
 {
@@ -19,7 +20,7 @@ final class WebserviceFactory
         return new CacheDecorator($skautis->getWebService($name), new ArrayCache());
     }
 
-    public static function create(string $name, Skautis $skautis): CacheDecorator
+    public static function create(string $name, Skautis $skautis): WebService
     {
         return $skautis->getWebService($name);
     }
