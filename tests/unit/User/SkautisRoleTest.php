490bc2dd2d1b45d259c236ac727832881da52f83
František Maša <frantisekmasa1@gmail.com>
2018-03-23 01:32:58 +0100

    Add SkautisRole VO to replace stdClass usage
    This provides better API - is* methods instead of *strpos*ing the key property

---
+++ tests/unit/User/SkautisRoleTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Model\User;
+
+use Codeception\Test\Unit;
+
+final class SkautisRoleTest extends Unit
+{
+
+    public function testGetUnitId(): void
+    {
+        $role = new SkautisRole('vedouciStredisko', 123);
+
+        $this->assertSame(123, $role->getUnitId());
+    }
+
+    public function testBasicUnitLeader(): void
+    {
+        $role = new SkautisRole('vedouciStredisko', 123);
+
+        $this->assertTrue($role->isLeader());
+        $this->assertFalse($role->isAccountant());
+        $this->assertFalse($role->isOfficer());
+
+        $this->assertTrue($role->isBasicUnit());
+        $this->assertFalse($role->isTroop());
+    }
+
+    public function testBasicUnitAccountant(): void
+    {
+        $role = new SkautisRole('hospodarStredisko', 123);
+
+        $this->assertFalse($role->isLeader());
+        $this->assertTrue($role->isAccountant());
+        $this->assertFalse($role->isOfficer());
+
+        $this->assertTrue($role->isBasicUnit());
+        $this->assertFalse($role->isTroop());
+    }
+
+    public function testTroopOfficer(): void
+    {
+        $role = new SkautisRole('cinovnikOddil', 123);
+
+        $this->assertFalse($role->isLeader());
+        $this->assertFalse($role->isAccountant());
+        $this->assertTrue($role->isOfficer());
+
+        $this->assertFalse($role->isBasicUnit());
+        $this->assertTrue($role->isTroop());
+    }
+
+    public function testEmptyString(): void
+    {
+        $role = new SkautisRole('', 123);
+
+        $this->assertFalse($role->isLeader());
+        $this->assertFalse($role->isAccountant());
+        $this->assertFalse($role->isOfficer());
+
+        $this->assertFalse($role->isBasicUnit());
+        $this->assertFalse($role->isTroop());
+    }
+
+}
