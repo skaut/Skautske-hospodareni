9ee3259fd2ac7d3d8efac018e929620283ba0db3
František Maša <frantisekmasa1@gmail.com>
2018-03-26 16:07:32 +0200

    Add query & handler for all unit cashbooks
    Forward compatibility with multiple cashbooks for unit

---
+++ app/model/Cashbook/ReadModel/QueryHandlers/UnitCashbookListQueryHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Model\Cashbook\ReadModel\QueryHandlers;
+
+use Model\Cashbook\ObjectType;
+use Model\Cashbook\ReadModel\Queries\UnitCashbookListQuery;
+use Model\DTO\Cashbook\UnitCashbook;
+use Model\Skautis\Mapper;
+
+final class UnitCashbookListQueryHandler
+{
+
+    /** @var Mapper */
+    private $mapper;
+
+    public function __construct(Mapper $mapper)
+    {
+        $this->mapper = $mapper;
+    }
+
+    /**
+     * @return UnitCashbook[]
+     */
+    public function handle(UnitCashbookListQuery $query): array
+    {
+        $cashbookId = $this->mapper->getLocalId($query->getUnitId(), ObjectType::UNIT);
+
+        return [
+            new UnitCashbook($cashbookId),
+        ];
+    }
+
+}
