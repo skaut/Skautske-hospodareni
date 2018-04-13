eb14b92fb9ba54eeb7447a65f1730f0a324209ea
František Maša <frantisekmasa1@gmail.com>
2018-04-13 16:03:53 +0200

    Remove unused methods

--- app/model/Chit/ChitTable.php
+++ app/model/Chit/ChitTable.php
@@ -2,41 +2,17 @@
 
 namespace Model;
 
-use Dibi\Row;
-
 class ChitTable extends BaseTable
 {
-
-    /**
-     * vrací konretní paragon
-     * @param int $chitId
-     * @return Row|FALSE
-     */
-    public function get($chitId)
-    {
-        return $this->connection->fetch("SELECT * FROM [" . self::TABLE_CHIT_VIEW . "] WHERE id=%i", $chitId);
-    }
-
-    /**
-     * vrací seznam poragonů podle zadaných ID
-     * @param int $localEventId
-     * @param array $list - seznam id
-     * @return array
-     */
-    public function getIn($localEventId, array $list)
-    {
-        return $this->connection->fetchAll("SELECT * FROM [" . self::TABLE_CHIT_VIEW . "] WHERE eventId=%i", $localEventId, " AND id in %in", $list, "ORDER BY date");
-    }
 
     /**
      * aktualizuje paragon podle $id
      * @param int $chitId
      * @param array $values
-     * @return bool
      */
-    public function update($chitId, $values): bool
+    public function update($chitId, $values): void
     {
-        return (bool)$this->connection->query("UPDATE [" . self::TABLE_CHIT . "] SET ", $values, "WHERE id=%i", $chitId);
+        $this->connection->query("UPDATE [" . self::TABLE_CHIT . "] SET ", $values, "WHERE id=%i", $chitId);
     }
 
     /**
