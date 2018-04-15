b9af519bbb54474c0868fa1897d0306e4d62a0c2
František Maša <frantisekmasa1@gmail.com>
2018-04-15 13:03:30 +0200

    Use SyslogUpdHandler to send logs to Papertrail

---
+++ app/model/Infrastructure/Services/SkautisCallListener.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Model\Infrastructure\Services;
+
+use Psr\Log\LoggerInterface;
+use Skautis\SkautisQuery;
+use Skautis\Wsdl\WebService;
+use Skautis\Wsdl\WsdlManager;
+
+final class SkautisCallListener
+{
+
+    /** @var LoggerInterface */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function register(WsdlManager $wsdlManager): void
+    {
+        $wsdlManager->addWebServiceListener((string) WebService::EVENT_SUCCESS, [$this, 'handleSuccess']);
+        $wsdlManager->addWebServiceListener((string) WebService::EVENT_FAILURE, [$this, 'handleError']);
+    }
+
+    public function handleSuccess(SkautisQuery $query): void
+    {
+        $this->logger->debug(
+            sprintf('Skautis query "%s" sucessfuly performed.', $query->fname),
+            $this->prepareContext($query)
+        );
+    }
+
+    public function handleError(SkautisQuery $query): void
+    {
+        $this->logger->debug(
+            sprintf('Skautis query "%s" returns error: %s', $query->fname, $query->getExceptionString()),
+            $this->prepareContext($query)
+        );
+    }
+
+    private function prepareContext(SkautisQuery $query): array
+    {
+        return [
+            'arguments' => $query->args,
+            'time' => $query->time,
+        ];
+    }
+
+}
