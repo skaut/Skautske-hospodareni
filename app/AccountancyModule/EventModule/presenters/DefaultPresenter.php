1ed127da09c1cf671103f9b7c3e005d4c36b5229
František Hána <sinacek@gmail.com>
2018-07-27 15:24:47 +0200

    add default state

--- app/AccountancyModule/EventModule/presenters/DefaultPresenter.php
+++ app/AccountancyModule/EventModule/presenters/DefaultPresenter.php
@@ -134,7 +134,7 @@ class DefaultPresenter extends BasePresenter
         }
     }
 
-    public function handleChangeState(?string $state) : void
+    public function handleChangeState(?string $state = null) : void
     {
         $this->ses->state = $state;
         if ($this->isAjax()) {
