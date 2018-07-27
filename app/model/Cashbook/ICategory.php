2d8dc98607c93ab1b295bdced889f5865004478c
František Hána <sinacek@gmail.com>
2018-07-27 15:26:00 +0200

    add new camp categories

--- app/model/Cashbook/ICategory.php
+++ app/model/Cashbook/ICategory.php
@@ -6,9 +6,11 @@ namespace Model\Cashbook;
 
 interface ICategory
 {
-    public const UNDEFINED_INCOME_ID  = 12;
-    public const UNDEFINED_EXPENSE_ID = 8;
-    public const CAMP_RESERVE_ID      = 15;
+    public const UNDEFINED_INCOME_ID     = 12;
+    public const UNDEFINED_EXPENSE_ID    = 8;
+    public const CAMP_RESERVE_ID         = 15;
+    public const CAMP_TRANSFER_TO_UNIT   = 7;
+    public const CAMP_TRANSFER_FROM_UNIT = 9;
 
     public function getId() : int;
 
