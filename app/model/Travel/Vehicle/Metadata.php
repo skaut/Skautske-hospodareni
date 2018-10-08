1ddc29a0533dda293044767e9a51105c5ced723d
František Maša <frantisekmasa1@gmail.com>
2018-10-08 19:07:15 +0200

    Mapped Travel\Vehicle via annotations

--- app/model/Travel/Vehicle/Metadata.php
+++ app/model/Travel/Vehicle/Metadata.php
@@ -4,12 +4,23 @@ declare(strict_types=1);
 
 namespace Model\Travel\Vehicle;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Embeddable()
+ */
 class Metadata
 {
-    /** @var \DateTimeImmutable */
+    /**
+     * @var \DateTimeImmutable
+     * @ORM\Column(type="datetime_immutable")
+     */
     private $createdAt;
 
-    /** @var string */
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
     private $authorName;
 
     public function __construct(\DateTimeImmutable $createdAt, string $authorName)
