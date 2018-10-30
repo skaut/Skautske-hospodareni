139eddb05afcd9a47bfd80f5a42686cdd3ebe056
František Hána <sinacek@gmail.com>
2018-10-30 19:51:54 +0100

    Add participant

---
+++ app/model/Participant/Factory/ParticipantFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Model\Skautis\Factory;
+
+use Cake\Chronos\Date;
+use Model\Participant\Participant;
+use Money\Currency;
+use Money\Money;
+use function preg_match;
+
+final class ParticipantFactory
+{
+    public static function create(\stdClass $skautisParticipant) : Participant
+    {
+        preg_match('/(?P<last>\S+)\s+(?P<first>[^(]+)(\((?P<nick>.*)\))?.*/', $skautisParticipant->Person, $matches);
+
+        return new Participant(
+            $matches['first'],
+            $matches['last'],
+            $matches['nick'] ?? null,
+            $skautisParticipant->Age,
+            new Date($skautisParticipant->Birthday),
+            $skautisParticipant->Street,
+            $skautisParticipant->City,
+            (int) $skautisParticipant->Postcode,
+            $skautisParticipant->State,
+            (int) $skautisParticipant->ID_Unit,
+            $skautisParticipant->Unit,
+            $skautisParticipant->UnitRegistrationNumber,
+            (int) $skautisParticipant->Days,
+            new Money($skautisParticipant->Note ?? 0, new Currency('CZK'))
+        );
+    }
+}
---
+++ app/model/Participant/Participant.php
@@ -0,0 +1,160 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Model\Participant;
+
+use Cake\Chronos\Date;
+use Money\Money;
+
+class Participant
+{
+    /** @var string */
+    private $firstName;
+
+    /** @var string */
+    private $lastName;
+
+    /** @var string|null */
+    private $nickName;
+
+    /** @var int */
+    private $age;
+
+    /** @var Date */
+    private $birthday;
+
+    /** @var string */
+    private $street;
+
+    /** @var string */
+    private $city;
+
+    /** @var int */
+    private $postcode;
+
+    /** @var string */
+    private $state;
+
+    /** @var int */
+    private $unitId;
+
+    /** @var string */
+    private $unit;
+
+    /** @var string */
+    private $unitRegistrationNumber;
+
+    /** @var int */
+    private $days;
+
+    /** @var Money|null */
+    private $payment;
+
+    public function __construct(
+        string $firstName,
+        string $lastName,
+        ?string $nickname,
+        int $age,
+        Date $birthday,
+        string $street,
+        string $city,
+        int $postcode,
+        string $state,
+        int $unitId,
+        string $unit,
+        string $unitRegistrationNumber,
+        int $days,
+        ?Money $payment
+    ) {
+        $this->firstName              = $firstName;
+        $this->lastName               = $lastName;
+        $this->nickName               = $nickname;
+        $this->age                    = $age;
+        $this->birthday               = $birthday;
+        $this->street                 = $street;
+        $this->city                   = $city;
+        $this->postcode               = $postcode;
+        $this->state                  = $state;
+        $this->unitId                 = $unitId;
+        $this->unit                   = $unit;
+        $this->unitRegistrationNumber = $unitRegistrationNumber;
+        $this->days                   = $days;
+        $this->payment                = $payment;
+    }
+
+    public function getFirstName() : string
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName() : string
+    {
+        return $this->lastName;
+    }
+
+    public function getNickName() : ?string
+    {
+        return $this->nickName;
+    }
+
+    public function getAge() : int
+    {
+        return $this->age;
+    }
+
+    public function getBirthday() : Date
+    {
+        return $this->birthday;
+    }
+
+    public function getStreet() : string
+    {
+        return $this->street;
+    }
+
+    public function getCity() : string
+    {
+        return $this->city;
+    }
+
+    public function getPostcode() : int
+    {
+        return $this->postcode;
+    }
+
+    public function getState() : string
+    {
+        return $this->state;
+    }
+
+    public function getUnitId() : int
+    {
+        return $this->unitId;
+    }
+
+    public function getUnit() : string
+    {
+        return $this->unit;
+    }
+
+    public function getUnitRegistrationNumber() : string
+    {
+        return $this->unitRegistrationNumber;
+    }
+
+    public function getDays() : int
+    {
+        return $this->days;
+    }
+
+    public function getPayment() : ?Money
+    {
+        return $this->payment;
+    }
+
+    public function getDisplayName() : string
+    {
+        return $this->lastName . ' ' . $this->firstName . ($this->nickName !== null ? '(' . $this->nickName . ')' : '');
+    }
+}
