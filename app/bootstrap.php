a032f33114281eb4540f0257a0f9a9c9a124a8bc
František Maša <frantisekmasa1@gmail.com>
2018-11-19 18:52:50 +0100

    Remove nette- prefix from log and temp dir

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-putenv('TMPDIR=' . dirname(__DIR__) . '/nette-temp');
+$tempDir = dirname(__DIR__) . '/temp';
+
+putenv('TMPDIR=' . $tempDir);
 
 $configurator = new Nette\Configurator();
 $configurator->setDebugMode(getenv('DEVELOPMENT_MACHINE') === 'true' ?: '94.113.119.27');
-$configurator->enableDebugger(__DIR__ . '/../nette-log');
-$configurator->setTempDirectory(__DIR__ . '/../nette-temp');
+$configurator->enableDebugger(__DIR__ . '/../log');
+$configurator->setTempDirectory($tempDir);
 
 $configurator->createRobotLoader()
     ->addDirectory(__DIR__)
