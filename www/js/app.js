10d63c0a81f0f4ad5f04fb556fdec1085db8a811
František Maša <frantisekmasa1@gmail.com>
2019-01-04 20:50:02 +0100

    Added logout time to new layout

---
+++ www/js/app.js
@@ -0,0 +1,30 @@
+(function () {
+    initializeLogoutTimer();
+})();
+
+function initializeLogoutTimer() {
+    let minutesLeft = 30;
+
+    const timer = document.getElementById('timer');
+    const minutesElement = document.getElementById('timer-minutes');
+
+    const decrementMinutes = () => {
+        minutesLeft--;
+
+        if (minutesLeft < 0) {
+            minutesElement.title = minutesElement.innerText = 'Byl jsi odhlášen!';
+            clearInterval(interval);
+            return;
+        }
+
+        if (minutesLeft === 10) {
+            timer.classList.add('bg-danger');
+        }
+
+        minutesElement.innerText = minutesLeft + ' min';
+        minutesElement.title = 'Do odhlášení zbývá ' + minutesLeft + ' min';
+    };
+
+    decrementMinutes();
+    const interval = setInterval(decrementMinutes, 60 * 1000);
+}
