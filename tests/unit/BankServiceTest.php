ee6a3f5ac7a722a50374c83849cedc4f941dd12a
František Hána <sinacek@gmail.com>
2019-01-28 23:04:01 +0100

    fixup! fix pairing

--- tests/unit/BankServiceTest.php
+++ tests/unit/BankServiceTest.php
@@ -90,4 +90,97 @@ final class BankServiceTest extends Unit
         $this->assertSame('note123', $transaction->getNote());
         $this->assertSame('František Maša', $transaction->getPayer());
     }
+
+    public function testPaymentIsPairedOnlyByJoinedAccount() : void
+    {
+        $groupId1       = 12;
+        $bankAccountId1 = 159;
+        $lastPairing    = new \DateTimeImmutable('- 5 days');
+
+        $group1 = m::mock(Group::class, [
+            'getId' => $groupId1,
+            'getBankAccountId' => $bankAccountId1,
+            'getLastPairing' => $lastPairing,
+            'updateLastPairing' => null,
+        ]);
+
+        $groupId2       = 34;
+        $bankAccountId2 = 357;
+
+        $group2 = m::mock(Group::class, [
+            'getId' => $groupId2,
+            'getBankAccountId' => $bankAccountId2,
+            'getLastPairing' => $lastPairing,
+            'updateLastPairing' => null,
+        ]);
+
+        $groups = m::mock(IGroupRepository::class);
+        $groups->shouldReceive('findByIds')
+            ->once()
+            ->with([$groupId1, $groupId2])
+            ->andReturn([$groupId1 => $group1, $groupId2 => $group2]);
+        $groups->shouldReceive('save');
+
+        $bankAccount1 = m::mock(BankAccount::class, ['getToken' => '123']);
+        $bankAccount2 = m::mock(BankAccount::class, ['getToken' => '852']);
+
+        $bankAccounts = m::mock(IBankAccountRepository::class);
+        $bankAccounts->shouldReceive('find')
+            ->once()
+            ->with($bankAccountId1)
+            ->andReturn($bankAccount1);
+        $bankAccounts->shouldReceive('find')
+            ->once()
+            ->with($bankAccountId2)
+            ->andReturn($bankAccount2);
+
+        $today    = new \DateTimeImmutable();
+        $amount   = 200.50;
+        $vs1      = new VariableSymbol('123456');
+        $account  = (string) \Helpers::createAccountNumber();
+        $vs2      = new VariableSymbol('7854');
+        $account2 = (string) new BankAccount\AccountNumber('19', '2000145399', '0800');
+
+        $transactions1 = [
+            new Transaction('123', $today, $amount, $account, 'František Maša', $vs1->toInt(), null, null),
+        ];
+
+        $transactions2 = [
+            new Transaction('456', $today, $amount, $account2, 'František Maša', $vs2->toInt(), null, null),
+        ];
+        $bank          = m::mock(IFioClient::class);
+        $bank->shouldReceive('getTransactions')
+            ->once()
+            ->andReturn($transactions1);
+
+        $bank->shouldReceive('getTransactions')
+            ->once()
+            ->andReturn($transactions2);
+
+        $payments1 = [
+            new Payment($group1, '-', null, $amount, new \DateTimeImmutable(), $vs1, null, null, ''),
+        ];
+        \Helpers::assignIdentity($payments1[0], 1);
+
+        $payments2 = [
+            new Payment($group2, '-', null, $amount, new \DateTimeImmutable(), $vs2, null, null, ''),
+        ];
+        \Helpers::assignIdentity($payments2[0], 2);
+
+        $paymentRepository = m::mock(IPaymentRepository::class);
+        $paymentRepository->shouldReceive('findByMultipleGroups')
+            ->once()
+            ->with([$groupId1])
+            ->andReturn($payments1);
+        $paymentRepository->shouldReceive('findByMultipleGroups')
+            ->once()
+            ->with([$groupId2])
+            ->andReturn($payments2);
+        $paymentRepository->shouldReceive('saveMany');
+
+        $bankService = new BankService($groups, $bank, $paymentRepository, $bankAccounts);
+
+        $paired = $bankService->pairAllGroups([$groupId1, $groupId2]);
+        $this->assertSame(2, $paired);
+    }
 }
