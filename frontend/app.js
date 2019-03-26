5cf22dc5e4c5cf4c2de92106c9099370bad8ff04
František Maša <frantisekmasa1@gmail.com>
2019-03-26 16:19:12 +0100

    Added support for AJAX using Naja

--- frontend/app.js
+++ frontend/app.js
@@ -2,8 +2,10 @@ import 'bootstrap.native';
 import {dom} from './icons';
 import {initializeLogoutTimer} from './logoutTimer';
 import {toggleAllCheckboxes} from './utils';
+import naja from 'naja';
 
 dom.watch();
 initializeLogoutTimer();
 
+document.addEventListener('DOMContentLoaded', naja.initialize.bind(naja));
 document.toggleAllCheckboxes = toggleAllCheckboxes;
