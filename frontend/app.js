db51fdd76c48b1cd5766f77868da23e08b210da0
Vojtěch Káně <vojtech.kane@gmail.com>
2019-04-15 12:29:04 +0200

    Toggle main checkbox when dependent one changes

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,7 @@
 import 'bootstrap.native';
 import {dom} from './icons';
 import {initializeLogoutTimer} from './logoutTimer';
-import {toggleAllCheckboxes} from './utils';
+import {toggleAllCheckboxes, toggleMainCheckbox} from './utils';
 import naja from 'naja';
 import { initializeDatePicker } from "./js/datePicker";
 import moment from 'moment';
@@ -21,3 +21,4 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 document.toggleAllCheckboxes = toggleAllCheckboxes;
+document.toggleMainCheckbox = toggleMainCheckbox;
