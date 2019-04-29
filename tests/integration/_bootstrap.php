1af2835dd3c938b1b0e3470b7a031f96fc4ef241
František Maša <frantisekmasa1@gmail.com>
2019-04-29 17:54:59 +0200

    Added support for tests of Skautis calls

--- tests/integration/_bootstrap.php
+++ tests/integration/_bootstrap.php
@@ -1,3 +1,13 @@
 <?php
 
 declare(strict_types=1);
+
+use VCR\VCR;
+
+VCR::configure()
+    ->enableLibraryHooks(['soap'])
+    ->setCassettePath(__DIR__ . '/fixtures')
+    ->setMode('none')
+    ->setStorage('json');
+
+VCR::turnOn();
