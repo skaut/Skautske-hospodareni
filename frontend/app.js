d0ae1e668d58ebb0733851fd6892f9fcd41fc78e
František Maša <frantisekmasa1@gmail.com>
2019-05-20 01:20:17 +0200

    Added netteForms.js for new UI

--- frontend/app.js
+++ frontend/app.js
@@ -2,6 +2,7 @@ import 'bootstrap.native';
 import {dom} from './icons';
 import {initializeLogoutTimer} from './logoutTimer';
 import {toggleAllCheckboxes, toggleMainCheckbox} from './utils';
+import netteForms from 'nette-forms';
 import naja from 'naja';
 import { initializeDatePicker } from "./js/datePicker";
 import moment from 'moment';
@@ -11,6 +12,7 @@ import { ProgressBar } from "./js/ProgressBar";
 // Use czech language for dates
 moment.locale('cs');
 
+naja.formsHandler.netteForms = netteForms;
 naja.registerExtension(ProgressBar);
 
 document.addEventListener('DOMContentLoaded', () => {
