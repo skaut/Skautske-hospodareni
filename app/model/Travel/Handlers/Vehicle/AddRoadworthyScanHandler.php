532f5e2a10439aca9952d54b5ddbc68f1c03421c
František Maša <frantisekmasa1@gmail.com>
2019-06-03 19:02:45 +0200

    Added command & handler for roadworthies

---
+++ app/model/Travel/Handlers/Vehicle/AddRoadworthyScanHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Model\Travel\Handlers\Vehicle;
+
+use Model\Common\IScanStorage;
+use Model\Travel\Commands\Vehicle\AddRoadworthyScan;
+use Model\Travel\Repositories\IVehicleRepository;
+use Nette\Utils\Random;
+
+final class AddRoadworthyScanHandler
+{
+    private const PREFIX = 'roadworthies/';
+
+    /** @var IVehicleRepository */
+    private $vehicles;
+
+    /** @var IScanStorage */
+    private $scanStorage;
+
+    public function __construct(IVehicleRepository $vehicles, IScanStorage $scanStorage)
+    {
+        $this->vehicles    = $vehicles;
+        $this->scanStorage = $scanStorage;
+    }
+
+    public function __invoke(AddRoadworthyScan $command) : void
+    {
+        $vehicle = $this->vehicles->find($command->getVehicleId());
+
+        $path = self::PREFIX . $vehicle->getId() . Random::generate(6) . '-' . $command->getFileName();
+
+        $this->scanStorage->save($path, $command->getScanContents());
+
+        $vehicle->addRoadworthyScan($path);
+
+        $this->vehicles->save($vehicle);
+    }
+}
