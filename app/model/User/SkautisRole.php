a296e3fd65c8a6c74316bdf33a88c3f00d350b9d
František Hána <sinacek@gmail.com>
2019-06-23 12:02:19 +0200

    Allow event manager edit records

--- app/model/User/SkautisRole.php
+++ app/model/User/SkautisRole.php
@@ -8,9 +8,10 @@ use Nette\Utils\Strings;
 
 class SkautisRole
 {
-    private const LEADER_PREFIX     = 'vedouci';
-    private const ACCOUNTANT_PREFIX = 'hospodar';
-    private const OFFICER_PREFIX    = 'cinovnik';
+    private const LEADER_PREFIX        = 'vedouci';
+    private const ACCOUNTANT_PREFIX    = 'hospodar';
+    private const OFFICER_PREFIX       = 'cinovnik';
+    private const EVENT_MANAGER_PREFIX = 'spravceAkci';
 
     private const BASIC_UNIT_SUFFIX = 'Stredisko';
     private const TROOP_SUFFIX      = 'Oddil';
@@ -50,6 +51,11 @@ class SkautisRole
     public function isOfficer() : bool
     {
         return Strings::startsWith($this->key, self::OFFICER_PREFIX);
+    }
+
+    public function isEventManager() : bool
+    {
+        return Strings::startsWith($this->key, self::EVENT_MANAGER_PREFIX);
     }
 
     public function isBasicUnit() : bool
