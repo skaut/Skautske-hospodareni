857498897bec460718802475b71d5bfceb737e94
František Maša <frantisekmasa1@gmail.com>
2019-09-21 10:12:27 +0200

    Track releases in Sentry

--- app/model/Infrastructure/Log/Sentry/HubFactory.php
+++ app/model/Infrastructure/Log/Sentry/HubFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Model\Infrastructure\Log\Sentry;
 
 use Sentry\ClientBuilder;
+use Sentry\Options;
 use Sentry\State\Hub;
 use Sentry\State\Scope;
 
@@ -13,16 +14,17 @@ final class HubFactory
     /**
      * @param callable[] $eventProcessors
      */
-    public static function create(?string $dsn, array $eventProcessors) : Hub
+    public static function create(?string $dsn, array $eventProcessors, string $releaseHash) : Hub
     {
-        $client = ClientBuilder::create(['dsn' => $dsn])->getClient();
-
         $scope = new Scope();
 
         foreach ($eventProcessors as $processor) {
             $scope->addEventProcessor($processor);
         }
 
-        return new Hub($client, $scope);
+        $options = new Options(['dsn' => $dsn]);
+        $options->setRelease($releaseHash);
+
+        return new Hub((new ClientBuilder($options))->getClient(), $scope);
     }
 }
