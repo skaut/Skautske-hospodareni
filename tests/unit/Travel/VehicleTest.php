a061618206f12e99e671633be2c6d0647b6f0fea
František Hána <sinacek@gmail.com>
2020-01-12 15:06:33 +0100

    remove check if it is a subunit for vehicles

--- tests/unit/Travel/VehicleTest.php
+++ tests/unit/Travel/VehicleTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Model\Travel;
 
 use DateTimeImmutable;
-use InvalidArgumentException;
 use Mockery as m;
 use Model\Common\FilePath;
 use Model\Common\ScanNotFound;
@@ -16,26 +15,14 @@ class VehicleTest extends \Codeception\Test\Unit
 {
     public function testCreateWithSubunit() : void
     {
-        $unit    = m::mock(Unit::class, ['getId' => 10]);
-        $subunit = m::mock(Unit::class, ['getId' => 20]);
-        $subunit->shouldReceive('isSubunitOf')->with($unit)->once()->andReturn(true);
+        $unit     = m::mock(Unit::class, ['getId' => 10]);
+        $subunit  = m::mock(Unit::class, ['getId' => 20]);
         $metadata = new Metadata(new DateTimeImmutable(), 'František Maša');
         $vehicle  = new Vehicle('test', $unit, $subunit, '666-666', 6.0, $metadata);
 
         $this->assertSame(10, $vehicle->getUnitId());
         $this->assertSame(20, $vehicle->getSubunitId());
         $this->assertTrue($metadata->equals($vehicle->getMetadata()));
-    }
-
-    public function testCantCreateWithUnrelatedSubunit() : void
-    {
-        $unit    = m::mock(Unit::class, ['getId' => 10]);
-        $subunit = m::mock(Unit::class, ['getId' => 20]);
-        $subunit->shouldReceive('isSubunitOf')->with($unit)->once()->andReturn(false);
-
-        $this->expectException(InvalidArgumentException::class);
-
-        new Vehicle('test', $unit, $subunit, '666-666', 6.0, new Metadata(new DateTimeImmutable(), 'František Maša'));
     }
 
     public function testAddRoadworthyScan() : void
