4bf991ed6b71b5cab19fa4e841930c4f0ca0dafa
František Hána <sinacek@gmail.com>
2020-09-28 14:30:23 +0200

    check participants

--- app/AccountancyModule/EventModule/presenters/CashbookPresenter.php
+++ app/AccountancyModule/EventModule/presenters/CashbookPresenter.php
@@ -43,7 +43,7 @@ class CashbookPresenter extends BasePresenter
     protected function startup() : void
     {
         parent::startup();
-        $isDraft          = $this->event !== null && $this->event->getState() === 'draft';
+        $isDraft          = $this->event->getState() === 'draft';
         $this->isEditable = $isDraft && $this->authorizator->isAllowed(Event::UPDATE_PARTICIPANT, $this->aid);
     }
 
