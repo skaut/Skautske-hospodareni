9da5b8b911bf445af53b1c44d08c269fee246303
František Hána <sinacek@gmail.com>
2020-11-30 13:36:40 +0100

    Update app/model/DTO/Payment/RepaymentFactory.php
    Co-authored-by: František Maša <frantisekmasa1@gmail.com>

--- app/model/DTO/Payment/RepaymentFactory.php
+++ app/model/DTO/Payment/RepaymentFactory.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Model\DTO\Payment;
 
-class RepaymentFactory
+class RepaymentCandidateFactory
 {
     public static function create(Payment $payment) : RepaymentCandidate
     {
