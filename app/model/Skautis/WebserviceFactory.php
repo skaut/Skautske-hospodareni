a2ae91b5fa0a5963e89ecf4e3a1ea91e2814eab0
František Maša <frantisekmasa1@gmail.com>
2021-01-08 11:26:30 +0100

    Drop unused factory

--- app/model/Skautis/WebserviceFactory.php
+++ app/model/Skautis/WebserviceFactory.php
@@ -5,9 +5,7 @@ declare(strict_types=1);
 namespace Model\Skautis;
 
 use Model\Infrastructure\Skautis\LazyWebService;
-use Nette\Caching\IStorage;
 use Nette\StaticClass;
-use Skautis\Nette\CacheAdapter;
 use Skautis\Skautis;
 use Skautis\Wsdl\Decorator\Cache\ArrayCache;
 use Skautis\Wsdl\Decorator\Cache\CacheDecorator;
@@ -22,14 +20,6 @@ final class WebserviceFactory
     public static function createCached(string $name, Skautis $skautis): CacheDecorator
     {
         return new CacheDecorator(self::create($name, $skautis), new ArrayCache());
-    }
-
-    public static function createOrg(Skautis $skautis, IStorage $storage): CacheDecorator
-    {
-        $cache = new CacheAdapter($storage, 'skautis-org');
-        $cache->setExpiration('2 hours');
-
-        return new CacheDecorator(self::create('org', $skautis), $cache);
     }
 
     public static function create(string $name, Skautis $skautis): LazyWebService
