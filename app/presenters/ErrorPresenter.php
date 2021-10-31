99e99cee01e3e681a5e29b45905d68fb13e70710
František Hána <sinacek@gmail.com>
2021-10-31 12:29:57 +0100

    fix type

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -77,7 +77,7 @@ class ErrorPresenter extends Presenter
         } elseif ($exception instanceof Nette\Application\BadRequestException) {
             $code = $exception->getCode();
             // load template 403.latte or 404.latte or ... 4xx.latte
-            $this->setView(in_array($code, [403, 404, 405, 410, 500], true) ? $code : '4xx');
+            $this->setView(in_array($code, [403, 404, 405, 410, 500], true) ? (string) $code : '4xx');
         } else {
             $this->setView('500'); // load template 500.latte
             $this->logger->critical($exception->getMessage(), ['exception' => $exception]);
