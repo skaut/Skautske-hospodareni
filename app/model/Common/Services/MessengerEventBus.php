bc19ceac22c6a562a4783d152217e06fff37e85d
František Hána <sinacek@gmail.com>
2022-01-09 10:32:26 +0100

    handle HandlerFailedException

--- app/model/Common/Services/MessengerEventBus.php
+++ app/model/Common/Services/MessengerEventBus.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Model\Common\Services;
 
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 final class MessengerEventBus implements EventBus
@@ -17,6 +18,10 @@ final class MessengerEventBus implements EventBus
 
     public function handle(object $event): void
     {
-        $this->eventBus->dispatch($event);
+        try {
+            $this->eventBus->dispatch($event);
+        } catch (HandlerFailedException $e) {
+            throw $e->getPrevious();
+        }
     }
 }
