494ced754a48434617b7445914c8827a94d8a80a
František Hána <sinacek@gmail.com>
2023-01-15 13:57:18 +0100

    fix exceptions

--- app/model/Skautis/Exception/MissingCurrentRole.php
+++ app/model/Skautis/Exception/MissingCurrentRole.php
@@ -8,4 +8,8 @@ use Exception;
 
 final class MissingCurrentRole extends Exception
 {
+    public function __construct(string $message = '', int $code = 0, Throwable|null $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
