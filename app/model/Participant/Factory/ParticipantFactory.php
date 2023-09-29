a397d5d4bdf4c59a462eb723bc1a45ea19de86fc
Marek Dědič <developer@dedic.eu>
2023-09-29 20:38:32 +0200

    Fixed education participant birthday parsing

--- app/model/Participant/Factory/ParticipantFactory.php
+++ app/model/Participant/Factory/ParticipantFactory.php
@@ -10,6 +10,7 @@ use Model\Participant\Payment;
 use stdClass;
 
 use function preg_match;
+use function property_exists;
 
 final class ParticipantFactory
 {
@@ -25,7 +26,13 @@ final class ParticipantFactory
             $matches['last'],
             $matches['nick'] ?? null,
             $skautisParticipant->Age ?? null,
-            isset($skautisParticipant->Birthday) ? new Date($skautisParticipant->Birthday) : null,
+            property_exists($skautisParticipant, 'Birthday')
+                ? new Date($skautisParticipant->Birthday)
+                : (
+                    property_exists($skautisParticipant, 'PersonBirthday')
+                        ? new Date($skautisParticipant->PersonBirthday)
+                        : null
+                ),
             $skautisParticipant->Street ?? $skautisParticipant->PersonAddressStreet,
             $skautisParticipant->City ?? $skautisParticipant->PersonAddressCity,
             (int) ($skautisParticipant->Postcode ?? $skautisParticipant->PersonAddressPostcode),
