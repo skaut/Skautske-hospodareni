f31274ca162e302efc3c8d39a6f1d27fa950a1d4
Marek Dědič <developer@dedic.eu>
2023-10-08 12:21:08 +0200

    Fixed migration timestamp in class name

--- migrations/2023/Version20231008115940.php
+++ migrations/2023/Version20231008115940.php
@@ -7,7 +7,7 @@ namespace Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20230928180113 extends AbstractMigration
+final class Version20231008115940 extends AbstractMigration
 {
     public function getDescription(): string
     {
