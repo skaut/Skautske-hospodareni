7118d7063051dd377c0cf053e0deb35cf6bdeaa6
Dorazil <dorazil@webwings.cz>
2023-10-23 14:46:17 +0200

    remove cleanup sql. Add fixtures sql file

---
+++ tests/_data/acceptance_init.sql
@@ -0,0 +1,4 @@
+INSERT INTO google_oauth (id, unit_id, email, token, updated_at)
+VALUES ('42288e92-27fb-453c-9904-36a7ebd14fe2', 27266, 'test@hospodareni.loc', '1//02yV7BM31saaQCgYIAPOOREPSNwF-L9Irbcw-iJEHRUnfxt2KULTjXQkPI-jl8LEN-SwVp6OybduZT21RiDf7RZBA4ZoZu86UXC8', '2017-06-15 00:00:00');
+INSERT INTO pa_bank_account (name, unit_id, token, created_at, allowed_for_subunits, number_prefix, number_number, number_bank_code)
+VALUES ('Acceptance', 27266, NULL, '2017-08-24 00:00:00', 1, NULL, '2000942144', '2010');
