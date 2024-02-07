8687be95e611b5f4cdcd0d1980d29a07a4bc8985
Marek Dědič <developer@dedic.eu>
2024-02-07 17:39:44 +0100

    Merged education accountant and regular accountant functions

--- app/model/User/UserService.php
+++ app/model/User/UserService.php
@@ -144,7 +144,7 @@ class UserService
                 ];
             }
 
-            if ($role->isLeader() || $role->isAccountant() || $role->isEventManager() || $role->isEducationLeader() || $role->isEducationAccountant()) {
+            if ($role->isLeader() || $role->isAccountant() || $role->isEventManager() || $role->isEducationLeader()) {
                 return [
                     self::ACCESS_READ => $unitIds,
                     self::ACCESS_EDIT => $unitIds,
