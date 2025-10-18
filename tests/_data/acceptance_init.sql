2f5af6e331a1c5832d135bd737080ce0e728bd91
Dorazil <dorazil@webwings.cz>
2025-10-19 00:01:03 +0200

    Add acceptance tests for Travel, Vehicle, and Contract
    Introduce CampCest and TravelCest classes with detailed acceptance test cases. Add functionalities to handle vehicles, contracts, and travel orders creation, modification, and deletion. Updated `acceptance_init.sql` for improved test data compatibility by adding ON DUPLICATE KEY handling in `google_oauth`.

--- tests/_data/acceptance_init.sql
+++ tests/_data/acceptance_init.sql
@@ -1,4 +1,12 @@
+-- google_oauth
 INSERT INTO google_oauth (id, unit_id, email, token, updated_at)
-VALUES ('42288e92-27fb-453c-9904-36a7ebd14fe2', 27266, 'test@hospodareni.loc', '1//02yV7BM31saaQCgYIAPOOREPSNwF-L9Irbcw-iJEHRUnfxt2KULTjXQkPI-jl8LEN-SwVp6OybduZT21RiDf7RZBA4ZoZu86UXC8', '2017-06-15 00:00:00');
+VALUES ('42288e92-27fb-453c-9904-36a7ebd14fe2', 27266, 'test@hospodareni.loc',
+        '1//02yV7BM31saaQCgYIAPOOREPSNwF-L9Irbcw-iJEHRUnfxt2KULTjXQkPI-jl8LEN-SwVp6OybduZT21RiDf7RZBA4ZoZu86UXC8',
+        '2017-06-15 00:00:00')
+    ON DUPLICATE KEY UPDATE
+                         unit_id    = VALUES(unit_id),
+                         email      = VALUES(email),
+                         token      = VALUES(token),
+                         updated_at = VALUES(updated_at);
 INSERT INTO pa_bank_account (name, unit_id, token, created_at, allowed_for_subunits, number_prefix, number_number, number_bank_code)
 VALUES ('Acceptance', 27266, NULL, '2017-08-24 00:00:00', 1, NULL, '2000942144', '2010');
