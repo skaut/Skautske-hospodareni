f399e7d48925dd1eb095cd413b5da743fa940fd9
dorazil <dorazil@webwings.cz>
2026-07-22 15:22:54 +0200

    refactor: replace `Kdyby\Replicator` with `Contributte\FormMultiplier` for improved functionality and updated dependency
    - Replaced deprecated `Kdyby\Replicator` with `Contributte\FormMultiplier` across forms and components.
    - Updated composer dependencies to include the new library and remove the old one.
    - Adjusted relevant methods and tests to align with API changes in `Contributte\FormMultiplier`.

--- app/Component/Forms/CustomControlFactories.php
+++ app/Component/Forms/CustomControlFactories.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Component\Forms;
 
-use Kdyby\Replicator\Container;
+use Contributte\FormMultiplier\Multiplier;
 use NasExt\Forms\Controls\DependentSelectBox;
 use Nette\Forms\Control;
 
@@ -34,9 +34,9 @@ trait CustomControlFactories
         return $this[$name] = new DependentSelectBox($label, $parents);
     }
 
-    public function addDynamic(string $name, callable $factory, int $createDefault = 0, bool $forceDefault = false): Container
+    public function addDynamic(string $name, callable $factory, int $createDefault = 0, bool $forceDefault = false): Multiplier
     {
-        $control = new Container($factory, $createDefault, $forceDefault);
+        $control = new Multiplier($factory, $createDefault);
         $control->currentGroup = $this->currentGroup;
 
         return $this[$name] = $control;
