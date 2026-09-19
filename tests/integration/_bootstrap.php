5af0237030692efbf603a13a2df591dc4ba49d60
dorazil <dorazil@webwings.cz>
2026-09-19 14:51:22 +0200

    test: copy VCR fixtures to a temporary directory in integration tests
    - Added copying of `fixtures` to a temporary directory for VCR cassette storage.
    - Updated VCR configuration to use the new temporary path for cassettes.

--- tests/integration/_bootstrap.php
+++ tests/integration/_bootstrap.php
@@ -2,11 +2,15 @@
 
 declare(strict_types=1);
 
+use Nette\Utils\FileSystem;
 use VCR\VCR;
+
+$cassettePath = __DIR__.'/../../temp/vcr-fixtures';
+FileSystem::copy(__DIR__.'/fixtures', $cassettePath);
 
 VCR::configure()
     ->enableLibraryHooks(['soap'])
-    ->setCassettePath(__DIR__.'/fixtures')
+    ->setCassettePath($cassettePath)
     ->setMode(VCR::MODE_NONE)
     ->setStorage('json');
 
